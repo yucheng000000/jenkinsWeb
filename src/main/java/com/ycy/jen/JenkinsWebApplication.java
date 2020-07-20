2ebeb01445177a7a6c8cdc6d074bcc06132a9359
11249 <1124972970@qq.com>
2020-07-20 23:18:19 +0800

    Initial commit

---
+++ src/main/java/com/ycy/jen/JenkinsWebApplication.java
@@ -0,0 +1,11 @@
+package com.ycy.jen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsWebApplication.class,args);
+    }
+}
---
+++ src/main/java/com/ycy/jen/controller/JenDemo.java
@@ -0,0 +1,16 @@
+package com.ycy.jen.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jendemo")
+public class JenDemo {
+
+    @RequestMapping("/print")
+    public String print(){
+        String str = "Hello World!";
+        System.out.println(str);
+        return str;
+    }
+}
